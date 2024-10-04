2aff8f1b6215a493535abc0d36d969ad4a7a1507
12345678925125 <132468699+12345678925125@users.noreply.github.com>
2024-10-04 12:23:29 -0300

    j

---
+++ perguntas.js
@@ -0,0 +1,10 @@
+criaCartao(
+    ´copa do brasil´
+    ´O que copa do brasil?´,
+    ´ A copa do brasil e uma competiço de futebol onde so time brasileiro participa´
+)
+criaCartao(
+    ´netflix´
+    ´o que e a netflix?´
+    ´ela e um app onde voce paga por mes para poder assitir varios filme´,
+)
